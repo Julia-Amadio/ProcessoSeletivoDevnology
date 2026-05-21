5207e72f42b9d17128d9ca0df59116ac6e596584
Julia-Amadio <julia.amadio.ps@gmail.com>
2026-05-20 22:43:45 -0300

    style: adequando código ao PEP 8

--- app.py
+++ app.py
@@ -3,18 +3,15 @@ from flask import Flask, jsonify
 # Importa módulo de data/hora da bib padrão
 from datetime import datetime
 
-'''
-Cria instância da aplicação
-    __name__ é uma var que contém o nome do módulo atual.
-    Flask usa isto para saber onde está o arquivo raiz do projeto.
-'''
+# Cria instância da aplicação.
+# __name__ é uma variável que contém o nome do módulo atual.
+# Flask usa isto para saber onde está o arquivo raiz do projeto.
 app = Flask(__name__)
 
-'''
-Quando alguém faz GET em /health, essa função é executada e retorna um JSON. 
-O health check é onde o ECS (ou qualquer orquestrador) bate periodicamente 
-para saber se o container está vivo e respondendo.
-'''
+
+# Quando alguém faz GET em /health, essa função é executada e retorna um JSON.
+# O health check é onde o ECS (ou qualquer orquestrador) bate periodicamente
+# para saber se o container está vivo e respondendo.
 @app.route('/health')
 def health():
     return jsonify({
@@ -23,16 +20,15 @@ def health():
         "version": "1.0.0"
     })
 
+
 # Rota raiz, só confirma que a API está no ar.
 @app.route('/')
 def index():
     return jsonify({"message": "Trainee DevOps API"})
 
-'''
-Só executa o servidor se o arquivo for chamado diretamente. 
-0.0.0.0 faz com que escute em todas as interfaces de rede,
-necessário dentro de um container para que requisições externas cheguem.
-'''
 
+# Só executa o servidor se o arquivo for chamado diretamente.
+# 0.0.0.0 faz com que escute em todas as interfaces de rede,
+# necessário dentro de um container para que requisições externas cheguem.
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
